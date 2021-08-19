466879c5b5dcbfcbd41035b976b0ace5817d3de9
Ilshat Galimov <86232244+ilshat1kzn@users.noreply.github.com>
2021-08-19 16:45:55 +0300

    Create hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,14 @@
+// Программа, которая выводит "Hello, World!"
+
+using System;
+
+namespace Slide01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
